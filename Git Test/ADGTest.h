db040f0ebdf62ab12c9bf9617b9dce2dc7f37a8e
Adam Gould <AdamGould@Adams-MacBook-Pro.local>
2014-03-13 21:58:05 +0000

    added a test class

---
+++ Git Test/ADGTest.h
@@ -0,0 +1,13 @@
+//
+//  ADGTest.h
+//  Git Test
+//
+//  Created by Adam Gould on 13/03/2014.
+//  Copyright (c) 2014 Adam Gould. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADGTest : NSObject
+
+@end
---
+++ Git Test/ADGTest.m
@@ -0,0 +1,13 @@
+//
+//  ADGTest.m
+//  Git Test
+//
+//  Created by Adam Gould on 13/03/2014.
+//  Copyright (c) 2014 Adam Gould. All rights reserved.
+//
+
+#import "ADGTest.h"
+
+@implementation ADGTest
+
+@end
